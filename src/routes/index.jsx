d56c96f54f24330fafe1d5e3371472aabb7f262f
Austin Gil <hey@austingil.com>
2023-04-13 14:14:47 -0700

    Streaming chat complete

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,139 +1,95 @@
 import server$, { createServerAction$, eventStream } from 'solid-start/server';
 import { createEffect, createSignal, onCleanup, Show, onMount } from 'solid-js';
+import { z } from 'zod';
+import { createParser } from 'eventsource-parser';
 import { openai, uploads } from '../services/index.js';
 import { Input, Button } from '../components';
-import { z } from 'zod';
 
-import {
-  createParser,
-  ParsedEvent,
-  ReconnectInterval,
-} from 'eventsource-parser';
-
-const serverFunction = server$(async function (form, request) {
-  const prompt = z.string().parse(form.get('prompt'));
-
-  const encoder = new TextEncoder();
+/**
+ * @see https://github.com/Nutlope/twitterbio/blob/main/utils/OpenAIStream.ts
+ * @param {Awaited<ReturnType<typeof openai.createCompletion>>} response
+ */
+function createOpenAIStream(response) {
   const decoder = new TextDecoder();
-
   let counter = 0;
 
-  const res = await openai.createChatCompletion(
-    {
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'user',
-          content: prompt,
-        },
-      ],
-      temperature: 0.7,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      max_tokens: 100,
-      stream: true,
-      n: 1,
-    },
-    { responseType: 'stream' }
-  );
-
-  // const res = await fetch('https://api.openai.com/v1/chat/completions', {
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ''}`,
-  //   },
-  //   method: 'POST',
-  //   body: JSON.stringify({
-  //     model: 'gpt-3.5-turbo',
-  //     messages: [
-  //       {
-  //         role: 'user',
-  //         content: prompt,
-  //       },
-  //     ],
-  //     temperature: 0.7,
-  //     top_p: 1,
-  //     frequency_penalty: 0,
-  //     presence_penalty: 0,
-  //     max_tokens: 100,
-  //     stream: true,
-  //     n: 1,
-  //   }),
-  // });
-
-  const stream = new ReadableStream({
+  return new ReadableStream({
     async start(controller) {
-      /** @param {ParsedEvent | ReconnectInterval} event */
+      /** @param {import('eventsource-parser').ParsedEvent | import('eventsource-parser').ReconnectInterval} event */
       function onParse(event) {
         if (event.type === 'event') {
           const data = event.data;
-          // https://beta.openai.com/docs/api-reference/completions/create#completions/create-stream
+
           if (data === '[DONE]') {
             controller.close();
             return;
           }
           try {
-            const json = JSON.parse(data);
             /**
-             * Chat Data:
-             * data: {"id":"chatcmpl-74e5W3iHLdixvxeqOUEHrOY0vzrN5","object":"chat.completion.chunk","created":1681341766,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"role":"assistant"},"index":0,"finish_reason":null}]}
-             *
              * Completion Data:
              * data: {"id": "cmpl-74e2oF4Hoqfz5duNHWKXjvM0ANCGp", "object": "text_completion", "created": 1681341598, "choices": [{"text": "\n", "index": 0, "logprobs": null, "finish_reason": null}], "model": "text-davinci-003"}
+             *
+             * Chat Completion Data:
+             * data: {"id":"chatcmpl-74e5W3iHLdixvxeqOUEHrOY0vzrN5","object":"chat.completion.chunk","created":1681341766,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"role":"assistant"},"index":0,"finish_reason":null}]}
              */
-            const text = json.choices[0].delta?.content || '';
+            const json = JSON.parse(data);
+            const text = json.choices[0].text || '';
+            // const text = json.choices[0].delta?.content || '';
             if (counter < 2 && (text.match(/\n/) || []).length > 0) {
-              // this is a prefix character (i.e., "\n\n"), do nothing
               return;
             }
-            const queue = encoder.encode(text);
-            controller.enqueue(queue);
+            controller.enqueue(text);
             counter++;
           } catch (error) {
-            // maybe parse error
             controller.error(error);
           }
         }
       }
 
-      // stream response (SSE) from OpenAI may be fragmented into multiple chunks
-      // this ensures we properly read chunks and invoke an event for each SSE event stream
       const parser = createParser(onParse);
-      // https://web.dev/streams/#asynchronous-iteration
-      for await (const chunk of res.data) {
-        console.log(chunk.toString());
+      for await (const chunk of response.data) {
         parser.feed(decoder.decode(chunk));
       }
     },
   });
+}
+
+const routeAction = server$(async function (formData) {
+  const prompt = z.string().parse(formData.get('prompt'));
+
+  const response = await openai.createCompletion(
+    {
+      model: 'text-davinci-003',
+      prompt: prompt,
+      temperature: 0,
+      max_tokens: 10,
+      stream: true,
+    },
+    { responseType: 'stream' }
+  );
+  const stream = createOpenAIStream(response);
 
   return new Response(stream);
 });
-
-/**
- * @see https://twitter.com/Steve8708/status/1644377554034503680
- * @see https://github.com/Nutlope/twitterbio/blob/main/utils/OpenAIStream.ts
- */
 
 export default function () {
   let [state, setState] = createSignal('');
   /**
    * @param {SubmitEvent} event
    */
-  async function handleClick(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
     const form = /** @type {HTMLFormElement} */ (event.target);
     const url = new URL(form.action);
     const response = await fetch(url, {
-      method: form.method,
+      method: 'POST', // form.method,
       body: new FormData(form),
     });
 
     if (!response.ok) {
       throw new Error(response.statusText);
     }
-    const data = response.body; // ReadableStream
+    const data = response.body;
     if (!data) {
       return;
     }
@@ -146,29 +102,14 @@ export default function () {
       const { value, done: doneReading } = await reader.read();
       done = doneReading;
       const chunkValue = decoder.decode(value);
-      console.log(chunkValue);
-      setState((previous) => {
-        return previous + chunkValue;
-      });
+      setState((previous) => previous + chunkValue);
     }
-
-    // createEffect(() => {
-    //   const eventSource = new EventSource(serverFunction.url);
-
-    //   eventSource.addEventListener('chat', (event) => {
-    //     console.log(event);
-    //     setState(event.data);
-    //   });
-
-    //   onCleanup(() => eventSource.close());
-    // });
-    event.preventDefault();
   }
 
   return (
     <main>
       {state()}
-      <form action={serverFunction.url} method="post" onSubmit={handleClick}>
+      <form action={routeAction.url} method="post" onSubmit={handleSubmit}>
         <Input
           label="Give me a prompt"
           name="prompt"
